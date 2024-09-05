651e618b7c1462ae299f1f2b476bb9804ba23b35
Neilson Flemming <Neilson.Flemming@Shopify.com>
2024-09-05 13:39:23 -0400

    intial commit

---
+++ extensions/local-pickup-generator/src/index.js
@@ -0,0 +1,21 @@
+// @ts-check
+
+/**
+ * @typedef {import("../generated/api").InputQuery} InputQuery
+ * @typedef {import("../generated/api").FunctionResult} FunctionResult
+ */
+
+/**
+ * @type {FunctionResult}
+ */
+const DELIVERY_OPTION = {
+  operations: [],
+};
+
+export default /**
+ * @param {InputQuery} input
+ * @returns {FunctionResult}
+ */
+(input) => {
+  return DELIVERY_OPTION;
+};
